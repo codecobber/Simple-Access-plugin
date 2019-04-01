3ddefb5616455547f19925e8af6ca239a99e0285
craig <craig@localhost.localdomain>
2019-04-01 14:36:32 +0100

    added button to reset.php

---
+++ simpleAccess/reset.php
@@ -0,0 +1,12 @@
+
+
+
+
+<?php
+echo "<h3>CAUTION!!!</h3>
+<p>Clicking the reset button will reset <strong>ALL</strong> of the user permissions back to the default setting.</p>
+<p><b>The default setting is as follows:</b><br> Each user has access <b>ONLY</b> to documents created by that user. Once reset, you will have to use the 'edit perms' button to add any new permissions to each account.'</p>
+";
+?>
+
+<button onclick="makeList()">Reset <strong>ALL</strong> user permissions</button>
