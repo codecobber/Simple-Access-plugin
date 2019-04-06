2548197b2f1c390226ff69aa0c31ba0886f2ef1b
codecobber <codecobber@gmail.com>
2019-04-06 18:52:40 +0100

    added styling

--- simpleAccess/overview.php
+++ simpleAccess/overview.php
@@ -10,7 +10,7 @@ echo "<div class='overView permsRow'><h3>Current user permissions</h3>";
 for($i=0;$i<$countItems;$i++){
   echo "
   <p class='overViewStyle'><i class='fas fa-user-circle'></i><b> User: </b>".$jsonGrab[$i]->id. "</p>
-  <p><i class='fas fa-compass'></i><b> Permissions: </b>".$jsonGrab[$i]->category."</p>";
+  <p><i class='fas fa-compass'></i><b> Permissions: </b>".$jsonGrab[$i]->category."<br>. . . .</p>";
 }
 echo "</div>";
 
